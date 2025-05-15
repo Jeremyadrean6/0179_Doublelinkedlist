20441905f6008ca2f9baee8b081ef21ad374b140
Jeremyadrean6 <bangje43121@gmail.com>
2025-05-15 11:03:45 +0700

    menambhkan step

--- doublelinkedlist1.cpp
+++ doublelinkedlist1.cpp
@@ -34,5 +34,55 @@ public:
         // Step 2. Assign value to the data fields
         newNode->noMhs = nim;
 
-    }
-}
+        // Step 3. Insert at beginning if list is empty or nim is smallest
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if(START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate Number not allowed" << endl;
+                return;
+            }
+            //step 4: newNode.next = START
+            newNode->next = START;
+
+            // Step 5. START.prev = newNode (if START exists)
+            if(START != NULL)
+               START->prev = newNode;
+            
+            // Step 6. newNode.prev = NULL
+            newNode->prev = NULL;
+
+            // Step 7. START =newNode
+            START = newNode;
+            return;
+        }
+
+        // Insert in between node
+        // Step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+
+        // Step 9: insert between current and current->next
+        newNode->next = current->next; // Step 9a: newNode.next = current.next
+        newNode->prev = current;       // Step 9b: newNode.prev = current
+
+        // insert last node 
+        if (current-> next != NULL)
+            current->next->prev = newNode; // Step 9c: current.next.prev = newNode
+
+        current->next = newNode; // Step 9d: current.next = newNode
+    }  
+    
+    
+};
+
+
