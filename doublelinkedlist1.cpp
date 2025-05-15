133c7a72668af0644e3a0e79080e0f23b3b76bbf
Jeremyadrean6 <bangje43121@gmail.com>
2025-05-15 10:10:22 +0700

    menambah void addnode

--- doublelinkedlist1.cpp
+++ doublelinkedlist1.cpp
@@ -21,5 +21,13 @@ public:
         START=NULL;
 
     }
-    
+    void addNode()
+    {
+        int nim;
+        string nm;
+        cout << "\nEnter the roll number of the student";
+        cin >> nim;
+
+ 
+    }  
 };
